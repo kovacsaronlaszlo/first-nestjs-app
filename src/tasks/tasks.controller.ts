862364a5aead15be0f5edb243245648cd121c6b2
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-10-22 23:07:23 +0200

    validation filtering and search dto

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,5 +1,5 @@
 import { CreateTaskDto } from './dto/create-task.dto';
-import { GetTasksFilterDto } from './dto/get-tasks.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 import {
   Body,

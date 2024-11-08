4f60120cbe7fd44f8e9baef6d468b571cb721bab
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-11-08 19:47:00 +0100

    restricting getting a task by id, stataus updates, and task deleting

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -34,10 +34,9 @@ export class TasksService {
     return tasks;
   }
 
-  async getTaskById(id: string): Promise<Task> {
-    console.log(id);
+  async getTaskById(id: string, user: User): Promise<Task> {
     const found = await this.tasksRepository.findOne({
-      where: { id: id },
+      where: { id, user },
     });
 
     if (!found) {
@@ -60,15 +59,19 @@ export class TasksService {
     return task;
   }
 
-  async deleteTask(id: string): Promise<void> {
-    const result = await this.tasksRepository.delete(id);
+  async deleteTask(id: string, user: User): Promise<void> {
+    const result = await this.tasksRepository.delete({ id, user });
     if (result.affected === 0) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
     }
   }
 
-  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
+  async updateTaskStatus(
+    id: string,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    const task = await this.getTaskById(id, user);
 
     task.status = status;
     await this.tasksRepository.save(task);

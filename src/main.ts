c62cd47d491035f675d1f03cd64ef0b441e3b4ad
kovacsaronlaszlo <k.aron.laszlo@gmail.com>
2024-11-15 23:03:50 +0100

    config changes

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
 
-  const port = 3000;
+  const port = process.env.PORT;
   await app.listen(port);
 
   logger.log(`Application listenetni on port ${port}`);
